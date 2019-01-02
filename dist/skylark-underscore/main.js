924c3966cdcaa78641655595ae795f46b3d2a2f1
hudaokeji <github@hudaokeji.com>
2019-01-02 23:27:56 +0900

    init commit

---
+++ dist/skylark-underscore/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-underscore - A version of underscore.js that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-underscore/
+ * @license MIT
+ */
+define(["./underscore"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./underscore"
+],function(_){
+	return _;
+});
